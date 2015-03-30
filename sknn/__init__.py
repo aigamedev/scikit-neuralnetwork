1703f803490f9955f91154019f1da94142927e72
Alex J. Champandard <alexjc@aigamedev.com>
2015-03-30 22:40:20 +0200

    Running autopep8 on the code to whip it into shape\!

--- sknn/__init__.py
+++ sknn/__init__.py
@@ -10,8 +10,6 @@ from pylearn2.costs.mlp.dropout import Dropout
 from pylearn2.training_algorithms.learning_rule import AdaGrad, RMSProp, Momentum
 
 
-
-
 class NeuralNetwork(object):
 
     """
@@ -21,7 +19,13 @@ class NeuralNetwork(object):
     you have access all your data upfront
     """
 
-    def __init__(self, dropout=False, input_scaler=None, output_scaler=None, learning_rate=0.001, verbose=0):
+    def __init__(
+            self,
+            dropout=False,
+            input_scaler=None,
+            output_scaler=None,
+            learning_rate=0.001,
+            verbose=0):
         """
         :param layers: List of tuples of types of layers alongside the number of neurons
         :param learning_rate: The learning rate for all layers
@@ -48,12 +52,14 @@ class NeuralNetwork(object):
         #self.learning_rule = None
         #self.learning_rule = RMSProp()
 
-
     def init_trainer(self):
-        return sgd.SGD(learning_rate=self.learning_rate, cost=self.cost, batch_size=1, learning_rule= self.learning_rule)
+        return sgd.SGD(
+            learning_rate=self.learning_rate,
+            cost=self.cost,
+            batch_size=1,
+            learning_rule=self.learning_rule)
 
-
-    def __scale(self,X,y):
+    def __scale(self, X, y):
         if self.input_normaliser is not None:
             X_s = self.input_normaliser.fit(X).transform(X)
         else:
@@ -65,7 +71,7 @@ class NeuralNetwork(object):
 
         return X_s, y_s
 
-    def __original_y(self,y):
+    def __original_y(self, y):
         if(self.output_normaliser is None):
             return y
         else:
@@ -77,23 +83,23 @@ class NeuralNetwork(object):
 
         pylearn2mlp_layers = []
         self.units_per_layer = []
-        #input layer units
-        self.units_per_layer+=[X.shape[1]]
+        # input layer units
+        self.units_per_layer += [X.shape[1]]
 
         layers = [
-                    #("RectifiedLinear", 200),
-                    #("RectifiedLinear", self.units_per_layer[0]*self.units_per_layer[0]),
-                    #("RectifiedLinear", self.units_per_layer[0]*20),
-                    ("RectifiedLinear", self.units_per_layer[0]*20),
-                    ("Linear", )]
+            #("RectifiedLinear", 200),
+            #("RectifiedLinear", self.units_per_layer[0]*self.units_per_layer[0]),
+            #("RectifiedLinear", self.units_per_layer[0]*20),
+            ("RectifiedLinear", self.units_per_layer[0] * 20),
+            ("Linear", )]
 
         self.layers = layers
 
         for layer in layers[:-1]:
-            self.units_per_layer+=[layer[1]]
+            self.units_per_layer += [layer[1]]
 
-        #Output layer units
-        self.units_per_layer+=[y.shape[1]]
+        # Output layer units
+        self.units_per_layer += [y.shape[1]]
 
         if(self.verbose > 0):
             print "Units per layer", str(self.units_per_layer)
@@ -101,9 +107,9 @@ class NeuralNetwork(object):
         for i, layer in enumerate(layers[:-1]):
 
             fan_in = self.units_per_layer[i] + 1
-            fan_out = self.units_per_layer[i+1]
+            fan_out = self.units_per_layer[i + 1]
             lim = np.sqrt(6) / (np.sqrt(fan_in + fan_out))
-            layer_name = "Hidden_%i_%s"%(i,layer[0])
+            layer_name = "Hidden_%i_%s" % (i, layer[0])
             activate_type = layer[0]
             if(i == 0):
                 first_hidden_name = layer_name
@@ -112,26 +118,26 @@ class NeuralNetwork(object):
                     dim=layer[1],
                     layer_name=layer_name,
                     irange=lim,
-                    W_lr_scale = self.weight_scale)
+                    W_lr_scale=self.weight_scale)
             elif activate_type == "Sigmoid":
                 hidden_layer = mlp.Sigmoid(
                     dim=layer[1],
                     layer_name=layer_name,
                     irange=lim,
-                    W_lr_scale = self.weight_scale)
+                    W_lr_scale=self.weight_scale)
             elif activate_type == "Tanh":
                 hidden_layer = mlp.Tanh(
                     dim=layer[1],
                     layer_name=layer_name,
                     irange=lim,
-                    W_lr_scale = self.weight_scale)
+                    W_lr_scale=self.weight_scale)
             elif activate_type == "Maxout":
                 hidden_layer = maxout.Maxout(
                     num_units=layer[1],
                     num_pieces=layer[2],
                     layer_name=layer_name,
                     irange=lim,
-                    W_lr_scale = self.weight_scale)
+                    W_lr_scale=self.weight_scale)
 
             else:
                 raise NotImplementedError(
@@ -140,7 +146,7 @@ class NeuralNetwork(object):
             pylearn2mlp_layers += [hidden_layer]
 
         output_layer_info = layers[-1]
-        output_layer_name = "Output_%s"%output_layer_info[0]
+        output_layer_name = "Output_%s" % output_layer_info[0]
 
         # fan_in = self.units_per_layer[-2] + 1
         # fan_out = self.units_per_layer[-1]
@@ -151,26 +157,29 @@ class NeuralNetwork(object):
                 dim=self.units_per_layer[-1],
                 layer_name=output_layer_name,
                 irange=0.00001,
-                W_lr_scale = self.weight_scale)
+                W_lr_scale=self.weight_scale)
 
         if(self.cost is not None):
             if(self.cost == "Dropout"):
-                self.cost = Dropout( input_include_probs =  { first_hidden_name : 1.0 }, input_scales =  { first_hidden_name: 1. })
-                #exit()
+                self.cost = Dropout(
+                    input_include_probs={
+                        first_hidden_name: 1.0}, input_scales={
+                        first_hidden_name: 1.})
+                # exit()
 
         if(output_layer_info[0] == "LinearGaussian"):
             output_layer = mlp.LinearGaussian(
 
-                init_beta = 0.1,
-                min_beta = 0.001,
-                max_beta = 1000,
-                beta_lr_scale = None,
+                init_beta=0.1,
+                min_beta=0.001,
+                max_beta=1000,
+                beta_lr_scale=None,
 
                 dim=self.units_per_layer[-1],
                 layer_name=output_layer_name,
                 irange=0.1,
 
-                W_lr_scale = self.weight_scale)
+                W_lr_scale=self.weight_scale)
 
         pylearn2mlp_layers += [output_layer]
 
@@ -180,7 +189,6 @@ class NeuralNetwork(object):
         self.trainer.setup(self.mlp, self.ds)
         inputs = self.mlp.get_input_space().make_theano_batch()
         self.f = theano.function([inputs], self.mlp.fprop(inputs))
-
 
     def fit(self, X, y):
         """
@@ -197,15 +205,14 @@ class NeuralNetwork(object):
             self.trainer.setup(self.mlp, self.ds)
 
         ds = self.ds
-        X_s,y_s = self.__scale(X,y)
+        X_s, y_s = self.__scale(X, y)
         ds.X = X_s
         ds.y = y_s
         self.trainer.train(dataset=ds)
 
         return self
 
-
-    def predict(self, X, n_out = 1):
+    def predict(self, X, n_out=1):
         """
 
         :param X:
@@ -215,25 +222,20 @@ class NeuralNetwork(object):
         if(self.ds is None):
             self.linit(X, np.array([np.zeros(n_out)]))
 
-        X_s,_ = self.__scale(X, None)
-        y =  self.f(X_s)
+        X_s, _ = self.__scale(X, None)
+        y = self.f(X_s)
         y_s = self.__original_y(y)
 
         return y_s
 
-
     def __getstate__(self):
-
 
         self.ds.X = self.ds.X[0:2]
         self.ds.y = self.ds.y[0:2]
 
-
-
         d = dict(self.__dict__)
         del d['f']
         del d['trainer']
-
 
         return d
 
@@ -247,26 +249,26 @@ class NeuralNetwork(object):
 
 
 class IncrementalMinMaxScaler():
-    def __init__(self, feature_range=(-1.0,1.0)):
+
+    def __init__(self, feature_range=(-1.0, 1.0)):
         self.feature_range = feature_range
         self.changed = False
         self.init = False
         self.times = 0
 
-
-    def fit(self,X, y = None):
+    def fit(self, X, y=None):
         self.changed = False
         self.times += 1
         if (not self.init):
-            self.min_ = np.array(X[0],dtype = np.float64)
-            self.max_ = np.array(X[0],dtype = np.float64)
+            self.min_ = np.array(X[0], dtype=np.float64)
+            self.max_ = np.array(X[0], dtype=np.float64)
             self.data_min = self.min_
             self.data_max = self.max_
             self.init = True
         else:
-            X = np.array(X,ndmin=2)
-            X = np.append(X, [self.data_min], axis = 0)
-            X = np.append(X, [self.data_max], axis = 0)
+            X = np.array(X, ndmin=2)
+            X = np.append(X, [self.data_min], axis=0)
+            X = np.append(X, [self.data_max], axis=0)
 
         feature_range = self.feature_range
         data_min = np.min(X, axis=0)
@@ -292,12 +294,10 @@ class IncrementalMinMaxScaler():
         self.data_min = data_min
         return self
 
-
     def transform(self, X):
         assert (len(X.shape) == 2), X
-        transformed =  (X * self.scale_) + self.min_
+        transformed = (X * self.scale_) + self.min_
         return transformed
-
 
     def inverse_transform(self, X):
         """Undo the scaling of X according to feature_range.
@@ -307,5 +307,5 @@ class IncrementalMinMaxScaler():
         X : array-like with shape [n_samples, n_features]
             Input data that will be transformed.
         """
-        reverse_transformed =  (X - self.min_)/self.scale_
+        reverse_transformed = (X - self.min_) / self.scale_
         return reverse_transformed
