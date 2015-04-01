86692fad328d9c98000baf0c2522db65af229bbb
Alex J. Champandard <alexjc@aigamedev.com>
2015-04-01 14:36:55 +0200

    More tests for the convolution network, in particular non-square kernels that make up entire rows or columns of the input.

--- sknn/tests/test_conv.py
+++ sknn/tests/test_conv.py
@@ -9,14 +9,29 @@ from sknn.nn import NeuralNetwork
 
 class TestConvolution(unittest.TestCase):
 
-    def test_SquareKernel(self):
-        nn = NeuralNetwork(
-            layers=[
-                ("Convolution", 4, (2,2)),
-                ("Linear",),
-            ])
-
+    def _run(self, nn):
         a_in, a_out = np.zeros((8,16,16)), np.zeros((8,4))
         nn.fit(a_in, a_out)
         a_test = nn.predict(a_in)
         assert_equal(type(a_out), type(a_in))
+
+    def test_SquareKernel(self):
+        self._run(NeuralNetwork(
+            layers=[
+                ("Convolution", 4, (2,2)),
+                ("Linear",),
+            ]))
+
+    def test_VerticalKernel(self):
+        self._run(NeuralNetwork(
+            layers=[
+                ("Convolution", 4, (16,1)),
+                ("Linear",),
+            ]))
+
+    def test_HorizontalKernel(self):
+        self._run(NeuralNetwork(
+            layers=[
+                ("Convolution", 4, (1,16)),
+                ("Linear",),
+            ]))
