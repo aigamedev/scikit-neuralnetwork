36a94f526074800843bcd2dffcd11949b6fd9722
Alex J. Champandard <alexjc@aigamedev.com>
2015-04-01 11:02:40 +0200

    Removed the weight_scale parameter, exposed many other parameters explicitly, moved iterations into the main constructor so it works better with pipelines.

--- sknn/nn.py
+++ sknn/nn.py
@@ -37,10 +37,26 @@ class NeuralNetwork(BaseEstimator):
         Seed for the initialization of the neural network parameters (e.g.
         weights and biases).  This is fully deterministic.
 
+    learning_rule : str
+        Name of the learning rule used during stochastic gradient descent,
+        one of ('default', 'momentum', 'rmsprop') at the moment.    
+
     learning_rate : float
         Real number indicating the default/starting rate of adjustment for
         the weights during gradient descent.  Different learning rules may
         take this into account differently.
+
+    learning_momentum : float
+        Real number indicating the momentum factor to be used for the
+        learning rule 'momentum'.
+
+    batch_size : int
+        Number of training samples to group together when performing stochastic
+        gradient descent.  By default each sample is treated on its own.
+
+    iterations : int
+        The number of iterations of gradient descent to perform on the
+        neural network's weights when training with fit().
 
     dropout : bool
         Whether to use drop-out training for the inputs (jittering) and the
@@ -51,25 +67,36 @@ class NeuralNetwork(BaseEstimator):
             self,
             layers,
             random_state=None,
-            learning_rate=0.001,
+            learning_rule='default',
+            learning_rate=0.01,
+            learning_momentum=0.9,
+            batch_size=1,
+            iterations=1,
             dropout=False):
 
         self.layers = layers
-        self.units_per_layer = None
         self.seed = random_state
 
+        self.unit_counts = None
         self.mlp = None
         self.ds = None
         self.trainer = None
         self.f = None
 
         self.cost = "Dropout" if dropout else None
-        self.weight_scale = None
         self.learning_rate = learning_rate
-        
-        self.learning_rule = None
-        # self.learning_rule = Momentum(0.9)
-        # self.learning_rule = RMSProp()
+        self.batch_size = batch_size
+        self.iterations = iterations
+
+        if learning_rule == 'default':
+            self.learning_rule = None
+        elif learning_rule == 'momentum':
+            self.learning_rule = Momentum(learning_momentum)
+        elif learning_rule == 'rmsprop':
+            self.learning_rule = RMSProp()
+        else:
+            raise NotImplementedError(
+                "Learning rule type `%s` is not supported." % learning_rule)
 
     def _create_trainer(self):
         sgd.log.setLevel(logging.WARNING)
@@ -92,30 +119,26 @@ class NeuralNetwork(BaseEstimator):
             return mlp.RectifiedLinear(
                 dim=args[1],
                 layer_name=name,
-                irange=irange,
-                W_lr_scale=self.weight_scale)
+                irange=irange)
 
         if activation_type == "Sigmoid":
             return mlp.Sigmoid(
                 dim=args[1],
                 layer_name=name,
-                irange=irange,
-                W_lr_scale=self.weight_scale)
+                irange=irange)
 
         if activation_type == "Tanh":
             return mlp.Tanh(
                 dim=args[1],
                 layer_name=name,
-                irange=irange,
-                W_lr_scale=self.weight_scale)
+                irange=irange)
 
         if activation_type == "Maxout":
             return maxout.Maxout(
                 num_units=args[1],
                 num_pieces=args[2],
                 layer_name=name,
-                irange=irange,
-                W_lr_scale=self.weight_scale)
+                irange=irange)
 
         raise NotImplementedError(
             "Hidden layer type `%s` is not implemented." % activation_type)
@@ -126,8 +149,7 @@ class NeuralNetwork(BaseEstimator):
             return mlp.Linear(
                 dim=args[1],
                 layer_name=name,
-                irange=0.00001,
-                W_lr_scale=self.weight_scale)
+                irange=0.00001)
 
         if activation_type == "LinearGaussian":
             return mlp.LinearGaussian(
@@ -137,25 +159,24 @@ class NeuralNetwork(BaseEstimator):
                 beta_lr_scale=None,
                 dim=args[1],
                 layer_name=name,
-                irange=0.1,
-                W_lr_scale=self.weight_scale)
+                irange=0.1)
 
         raise NotImplementedError(
             "Output layer type `%s` is not implemented." % name)
 
     def _create_mlp(self, X, y):
         # Calculate and store all layer sizes.
-        self.units_per_layer = [X.shape[1]]
+        self.unit_counts = [X.shape[1]]
         for layer in self.layers[:-1]:
-            self.units_per_layer += [layer[1]]
-        self.units_per_layer += [y.shape[1]]
+            self.unit_counts += [layer[1]]
+        self.unit_counts += [y.shape[1]]
 
-        log.debug("Units per layer: %r.", self.units_per_layer)
+        log.debug("Units per layer: %r.", self.unit_counts)
 
         mlp_layers = []
         for i, layer in enumerate(self.layers[:-1]):
-            fan_in = self.units_per_layer[i] + 1
-            fan_out = self.units_per_layer[i + 1]
+            fan_in = self.unit_counts[i] + 1
+            fan_out = self.unit_counts[i + 1]
             lim = np.sqrt(6) / (np.sqrt(fan_in + fan_out))
 
             layer_name = "Hidden_%i_%s" % (i, layer[0])
@@ -163,13 +184,13 @@ class NeuralNetwork(BaseEstimator):
             mlp_layers.append(hidden_layer)
 
         output_layer_info = list(self.layers[-1])
-        output_layer_info.append(self.units_per_layer[-1])
+        output_layer_info.append(self.unit_counts[-1])
 
         output_layer_name = "Output_%s" % output_layer_info[0]
         output_layer = self._create_output_layer(output_layer_name, output_layer_info)
         mlp_layers.append(output_layer)
 
-        return mlp.MLP(mlp_layers, nvis=self.units_per_layer[0], seed=self.seed)
+        return mlp.MLP(mlp_layers, nvis=self.unit_counts[0], seed=self.seed)
 
     def initialize(self, X, y):
         assert not self.initialized,\
@@ -194,7 +215,7 @@ class NeuralNetwork(BaseEstimator):
         """
         return not (self.ds is None or self.trainer is None or self.f is None)
 
-    def fit(self, X, y, n_iter=10):
+    def fit(self, X, y):
         """Fit the neural network to the given data.
 
         Parameters
@@ -206,10 +227,6 @@ class NeuralNetwork(BaseEstimator):
         y : array-like, shape = [n_samples, n_outputs]
             Target values as real numbers, either as regression targets or
             label probabilities for classification.
-
-        n_iter : int
-            The number of iterations of gradient descent to perform on the
-            neural network's weights.
 
         Returns
         -------
@@ -223,7 +240,7 @@ class NeuralNetwork(BaseEstimator):
             self.initialize(X, y)
 
         self.ds.X, self.ds.y = X, y
-        for _ in range(n_iter):
+        for _ in range(self.iterations):
             self.trainer.train(dataset=self.ds)
 
         return self
@@ -243,9 +260,9 @@ class NeuralNetwork(BaseEstimator):
         """
 
         if not self.initialized:
-            assert self.units_per_layer is not None,\
+            assert self.unit_counts is not None,\
                 "The neural network has not been trained."
-            y = np.zeros((X.shape[0], self.units_per_layer[-1]))
+            y = np.zeros((X.shape[0], self.unit_counts[-1]))
             self.initialize(X, y)
 
         return self.f(X)
---
+++ sknn/tests/test_rules.py
@@ -0,0 +1,28 @@
+import unittest
+from nose.tools import (assert_is_not_none, assert_false, assert_raises, assert_equal)
+
+import numpy as np
+
+
+from sknn import NeuralNetwork
+
+
+class TestLearningRules(unittest.TestCase):
+
+    def test_default(self):
+        self._run(NeuralNetwork(layers=[("Linear",)],
+                                learning_rule='default'))
+
+    def test_momentum(self):
+        self._run(NeuralNetwork(layers=[("Linear",)],
+                                learning_rule='momentum'))
+
+    def test_rmsprop(self):
+        self._run(NeuralNetwork(layers=[("Linear",)],
+                                learning_rule='rmsprop'))
+
+    def _run(self, nn):
+        a_in, a_out = np.zeros((8,16)), np.zeros((8,4))
+        nn.fit(a_in, a_out)
+        a_test = nn.predict(a_in)
+        assert_equal(type(a_out), type(a_test))
