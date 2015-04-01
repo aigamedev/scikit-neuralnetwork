ef55f3b24fbdf195a0044811e1c8a243dc116c99
Alex J. Champandard <alexjc@aigamedev.com>
2015-04-01 09:55:21 +0200

    Simplified the tests for the deep networks, reusing much of the linear test code.

--- sknn/tests/test_deep.py
+++ sknn/tests/test_deep.py
@@ -7,11 +7,11 @@ import numpy as np
 from nose.tools import (assert_is_not_none)
 from sklearn.utils.testing import (assert_raises, assert_equal)
 
-
 from sknn import NeuralNetwork
 
+from . import test_linear
 
-class TestDeepNetwork(unittest.TestCase):
+class TestDeepNetwork(test_linear.TestLinearNetwork):
 
     def setUp(self):
         self.nn = NeuralNetwork(
@@ -22,28 +22,3 @@ class TestDeepNetwork(unittest.TestCase):
                 ("Tanh", 4),
                 ("Linear",),
             ])
-
-    def test_LifeCycle(self):
-        del self.nn
-
-    def test_InitializeManually(self):
-        a_in, a_out = np.zeros((8,16)), np.zeros((8,4))
-        self.nn.initialize(a_in, a_out)
-
-    def test_PredictUninitialized(self):
-        a_in = np.zeros((8,16))
-        assert_raises(AssertionError, self.nn.predict, a_in)
-
-    def test_PredictAutoInitialize(self):
-        a_in = np.zeros((8,16))
-        self.nn.initialize(a_in, a_in)
-        a_out = self.nn.predict(a_in)
-        assert_equal(type(a_out), type(a_in))
-
-    def test_FitAutoInitialize(self):
-        a_in, a_out = np.zeros((8,16)), np.zeros((8,4))
-        self.nn.fit(a_in, a_out)
-
-    def test_FitWrongSize(self):
-        a_in, a_out = np.zeros((7,16)), np.zeros((9,4))
-        assert_raises(AssertionError, self.nn.fit, a_in, a_out)
