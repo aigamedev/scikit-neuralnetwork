997747618d3749062537b878656e88d8dfd93718
Alex J. Champandard <alexjc@aigamedev.com>
2015-05-02 19:26:37 +0200

    Prototype of a derived dataset class that supports expanding batches from a ``scipy.sparse`` data-structure.
    Updates #23.

--- sknn/dataset.py
+++ sknn/dataset.py
@@ -1,74 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, unicode_literals, print_function)
+
 from pylearn2.datasets.dataset import Dataset
-from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
 
 from pylearn2.utils.iteration import (SequentialSubsetIterator,
-  FiniteDatasetIterator,
-  resolve_iterator_class)
+                                      FiniteDatasetIterator,
+                                      resolve_iterator_class)
+
 import functools
-import logging
-import numpy
-import warnings
-from pylearn2.space import CompositeSpace, Conv2DSpace, VectorSpace, IndexSpace
+
+from pylearn2.space import CompositeSpace, VectorSpace
 from pylearn2.utils import safe_zip
 
-try:
-    import scipy.sparse
-except ImportError:
-    warnings.warn("Couldn't import scipy.sparse")
 import theano
-import gzip
 floatX = theano.config.floatX
-logger = logging.getLogger(__name__)
 
 
-def getNVis(dataset):
-  print "Visible layers: ",
-  print dataset.X.shape[1]
-  return dataset.X.shape[1]
-
-def getOutputClasses(dataset):
-  print "Output classes: ",
-  print dataset.y.shape[1]
-  return dataset.y.shape[1]
-
-class SparseExpanderDataset(Dataset):
+class SparseDesignMatrix(Dataset):
     """
     SparseExpanderDataset takes a numpy/scipy sparse matrix and calls .todense()
     as the batches are passed out of the iterator.
     """
 
-
-    def __init__(self, X_path=None, y_path=None, from_scipy_sparse_dataset=None, zipped_npy=False):
-
-        self.X_path = X_path
-        self.y_path = y_path
-
-        if self.X_path != None:
-            if zipped_npy == True:
-                logger.info('... loading sparse data set from a zip npy file')
-                self.X = scipy.sparse.csr_matrix(
-                    numpy.load(gzip.open(X_path)), dtype=floatX)
-            else:
-                logger.info('... loading sparse data set from a npy file')
-                self.X = scipy.sparse.csr_matrix(
-                    numpy.load(X_path).item(), dtype=floatX)
-        else:
-            logger.info('... building from given sparse dataset')
-            self.X = from_scipy_sparse_dataset
-
-
-        if self.y_path != None:
-            if zipped_npy == True:
-                logger.info('... loading sparse data set from a zip npy file')
-                self.y = scipy.sparse.csr_matrix(
-                    numpy.load(gzip.open(y_path)), dtype=floatX).todense()
-            else:
-                logger.info('... loading sparse data set from a npy file')
-                self.y = scipy.sparse.csr_matrix(
-                    numpy.load(y_path).item(), dtype=floatX)
-        else:
-            logger.info('... building from given sparse dataset')
-            self.X = from_scipy_sparse_dataset
+    def __init__(self, X, y):
+        self.X = X.astype(floatX)
+        self.y = y.astype(floatX)
 
         self.data_n_rows = self.X.shape[0]
         self.num_examples = self.data_n_rows
@@ -76,25 +32,23 @@ class SparseExpanderDataset(Dataset):
         self.stochastic = False
         X_space = VectorSpace(dim=self.X.shape[1])
         X_source = 'features'
-        if y_path is None:
-            space = X_space
-            source = X_source
-        else:
-            if self.y.ndim == 1:
-                dim = 1
-            else:
-                dim = self.y.shape[-1]
-            y_space = VectorSpace(dim=dim)
-            y_source = 'targets'
 
-            space = CompositeSpace((X_space, y_space))
-            source = (X_source, y_source)
+        if self.y.ndim == 1:
+            dim = 1
+        else:
+            dim = self.y.shape[-1]
+        y_space = VectorSpace(dim=dim)
+        y_source = 'targets'
+
+        space = CompositeSpace((X_space, y_space))
+        source = (X_source, y_source)
 
         self.data_specs = (space, source)
         self.X_space = X_space
         self._iter_data_specs = (self.X_space, 'features')
 
-
+    def get_num_examples(self):
+        return self.num_examples
 
     def get_design_matrix(self):
         return self.X
@@ -105,12 +59,6 @@ class SparseExpanderDataset(Dataset):
         """
         self.iterator(mode='sequential', batch_size=batch_size)
         return self.next()
-
-    def get_batch_topo(self, batch_size):
-        """
-        method inherited from Dataset
-        """
-        raise NotImplementedError('Not implemented for sparse dataset')
 
     def get_data_specs(self):
         """
@@ -148,11 +96,15 @@ class SparseExpanderDataset(Dataset):
         if data_specs is None:
                 data_specs = self._iter_data_specs
 
-            # If there is a view_converter, we have to use it to convert
-            # the stored data for "features" into one that the iterator
-            # can return.
-        # if
-        self.conv_fn = lambda x: x.todense()
+        # TODO: If there is a view_converter, we have to use it to convert
+        # the stored data for "features" into one that the iterator can return.
+
+        def convertion_function(x):
+            print(type(x), x.dtype, x.shape)
+            return x.todense().astype(floatX)
+
+        self.conv_fn = convertion_function
+
         space, source = data_specs
         if isinstance(space, CompositeSpace):
             sub_spaces = space.components
@@ -179,17 +131,12 @@ class SparseExpanderDataset(Dataset):
         else:
             mode = resolve_iterator_class(mode)
 
-
+        subset_iterator = mode(self.X.shape[0], batch_size, num_batches, rng)
         return FiniteDatasetIterator(self,
-                                     mode(self.X.shape[0],
-                                          batch_size,
-                                          num_batches,
-                                          rng),
+                                     subset_iterator,
                                      data_specs=data_specs,
                                      return_tuple=return_tuple,
                                      convert=convert)
-
-
 
     def __iter__(self):
         return self
--- sknn/mlp.py
+++ sknn/mlp.py
@@ -35,6 +35,8 @@ from pylearn2.training_algorithms.learning_rule import RMSProp, Momentum, AdaGra
 from pylearn2.space import Conv2DSpace
 from pylearn2.termination_criteria import MonitorBased
 
+from .dataset import SparseDesignMatrix
+
 
 class ansi:
     BOLD = '\033[1;97m'
@@ -543,7 +545,10 @@ class MultiLayerPerceptron(sklearn.base.BaseEstimator):
             view = input_space.get_origin_batch(X.shape[0])
             return DenseDesignMatrix(topo_view=view, y=y), input_space
         else:
-            return DenseDesignMatrix(X=X, y=y), None
+            if all([isinstance(a, numpy.ndarray) for a in (X, y)]):
+                return DenseDesignMatrix(X=X, y=y), None
+            else:
+                return SparseDesignMatrix(X=X, y=y), None
 
     def _initialize(self, X, y):
         assert not self.is_initialized,\
@@ -643,10 +648,6 @@ class MultiLayerPerceptron(sklearn.base.BaseEstimator):
 
         if y.ndim == 1:
             y = y.reshape((y.shape[0], 1))
-        if not isinstance(X, numpy.ndarray):
-            X = X.toarray()
-        if not isinstance(y, numpy.ndarray):
-            y = y.toarray()
 
         if not self.is_initialized:
             self._initialize(X, y)
