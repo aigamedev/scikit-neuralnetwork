754b0cafe039302229766fd7e61555f256d0c8f2
Alex J. Champandard <alexjc@aigamedev.com>
2015-05-02 21:00:18 +0200

    Renamed the tests, 1D case supported by code but not possible with scipy.sparse matrices.

--- sknn/tests/test_types.py
+++ sknn/tests/test_types.py
@@ -6,17 +6,16 @@ from sknn.mlp import MultiLayerPerceptron as MLP
 from sknn.mlp import Layer as L
 
 
-class TestInputDataTypes(unittest.TestCase):
+class TestScipySparseMatrix(unittest.TestCase):
 
     def setUp(self):
         self.nn = MLP(layers=[L("Gaussian")], n_iter=1)
 
-    def test_FitSciPySparse(self):
+    def test_Fit2D(self):
         X, y = scipy.sparse.csr_matrix((8, 4)), scipy.sparse.csr_matrix((8, 4))
         self.nn._fit(X, y)
 
-    def test_PredictSciPySparse(self):
+    def test_Predict2D(self):
         X, y = scipy.sparse.csr_matrix((8, 4)), scipy.sparse.csr_matrix((8, 4))
         self.nn._fit(X, y)
         self.nn._predict(X)
-
