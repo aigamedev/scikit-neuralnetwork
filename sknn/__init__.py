6d20860e200ff6049b79913a102dad9c615a9dcc
Alex J. Champandard <alexjc@aigamedev.com>
2015-05-13 19:28:04 +0200

    Bumping version number for documentation before API changes are merged.

--- sknn/__init__.py
+++ sknn/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, unicode_literals, print_function)
 
-__author__ = 'ssamot, alexjc'
-__version__ = '0.1'
+__author__ = 'alexjc, ssamot'
+__version__ = '0.2'
 
 
 import os
