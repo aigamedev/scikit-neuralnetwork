3b9b1c6ec86e91907f2e43ba307d2de2d9bceb5f
Alex J. Champandard <alexjc@aigamedev.com>
2015-06-24 15:37:34 +0200

    Adding a data sample mutator that's called at each epoch during training, useful for jittering or augmenting datasets.

---
+++ sknn/tests/test_data.py
@@ -0,0 +1,35 @@
+import unittest
+from nose.tools import (assert_in, assert_raises, assert_equals)
+
+import logging
+
+import numpy
+from sknn.mlp import Regressor as MLPR
+from sknn.mlp import Layer as L, Convolution as C
+
+
+class TestDataAugmentation(unittest.TestCase):
+
+    def setUp(self):
+        self.called = 0
+        self.value = 1.0
+
+        self.nn = MLPR(
+                    layers=[L("Linear")],
+                    n_iter=1,
+                    batch_size=2,
+                    converter=self._conv_fn)
+
+    def _conv_fn(self, sample):
+        self.called += 1
+        sample[sample == 0.0] = self.value
+
+    def test_TestCalledOK(self):
+        a_in, a_out = numpy.zeros((8,16)), numpy.zeros((8,4))
+        self.nn._fit(a_in, a_out)
+        assert_equals(a_in.shape[0], self.called)
+
+    def test_DataIsUsed(self):
+        self.value = float("nan")
+        a_in, a_out = numpy.zeros((8,16)), numpy.zeros((8,4))
+        assert_raises(RuntimeError, self.nn._fit, a_in, a_out)
